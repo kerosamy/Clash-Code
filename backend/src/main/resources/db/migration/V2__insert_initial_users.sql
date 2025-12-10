ab5c7249b8b75b8a3750e2874bd8711d2771e019
kerolos samy <kerosamy42004@gmail.com>
2025-12-10 17:10:22 +0200

    [CLASHCODE-134] - add super admin

--- backend/src/main/resources/db/migration/V2__insert_initial_users.sql
+++ backend/src/main/resources/db/migration/V2__insert_initial_users.sql
@@ -1,6 +1,7 @@
 -- Insert initial users
 INSERT INTO users (username, email, password, role, max_rate, current_rate)
 VALUES
+    ('admin', 'admin@gmail.com', '$2a$10$B2dVFmw1ptPbc.eCtkwKO.5knKfvI7kKHbPysc68m6r6KoUbO94NW', 'SUPER_ADMIN', 1000, 200),
     ('el-nagar', 'elnagar@gmail.com', '$2a$10$B2dVFmw1ptPbc.eCtkwKO.5knKfvI7kKHbPysc68m6r6KoUbO94NW', 'ADMIN', 5, 4),
     ('kero', 'kero@gmail.com', '$2a$10$B2dVFmw1ptPbc.eCtkwKO.5knKfvI7kKHbPysc68m6r6KoUbO94NW', 'ADMIN', 1500, 1230),
     ('john', 'john@gmail.com', '$2a$10$B2dVFmw1ptPbc.eCtkwKO.5knKfvI7kKHbPysc68m6r6KoUbO94NW', 'USER', 350, 200),
