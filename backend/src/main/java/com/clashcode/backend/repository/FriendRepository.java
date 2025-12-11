1b3db35f51461e23d2f6cddd533967bbf72e19c9
kerolos samy <kerosamy42004@gmail.com>
2025-12-11 11:20:52 +0200

    [CLASHCODE-161] - apply rebase

--- backend/src/main/java/com/clashcode/backend/repository/FriendRepository.java
+++ backend/src/main/java/com/clashcode/backend/repository/FriendRepository.java
@@ -8,7 +8,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-import java.awt.print.Pageable;
+import org.springframework.data.domain.Pageable;
 import java.util.Optional;
 
 public interface FriendRepository extends JpaRepository<Friend, Long> {
