33218e32ae2fb4e71a037d9cfd29dceb816d26d6
kerolos samy <kerosamy42004@gmail.com>
2025-12-31 22:59:21 +0200

    finsh

--- frontend/src/services/ws.ts
+++ frontend/src/services/ws.ts
@@ -22,7 +22,7 @@ export class EnhancedWebSocketService {
     this.messageCallback = onMessage;
     this.statusCallback('connecting');
 
-    const WS_URL = 'ws://localhost:8080/ws';
+    const WS_URL = 'wss://fugally-nonrepatriable-belle.ngrok-free.dev/ws';
     // wss://fugally-nonrepatriable-belle.ngrok-free.dev/ws
     this.client = new Client({
       brokerURL: WS_URL,
