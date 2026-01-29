11fa24120ac13229e1a0341677a92d7390a110fa
kerolos samy <kerosamy42004@gmail.com>
2026-01-29 03:46:23 +0200

    [CLASHCODE-175] remove main app test

--- backend/src/test/java/com/clashcode/backend/BackendApplicationTest.java
+++ backend/src/test/java/com/clashcode/backend/BackendApplicationTest.java
@@ -1,13 +1,13 @@
-package com.clashcode.backend;
-
-import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
-
-class BackendApplicationTest {
-	@Test
-	void main_shouldStartApplicationWithoutErrors() {
-
-		assertDoesNotThrow(() -> BackendApplication.main(new String[]{}));
-	}
-}
+//package com.clashcode.backend;
+//
+//import org.junit.jupiter.api.Test;
+//
+//import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+//
+//class BackendApplicationTest {
+//	@Test
+//	void main_shouldStartApplicationWithoutErrors() {
+//
+//		assertDoesNotThrow(() -> BackendApplication.main(new String[]{}));
+//	}
+//}
