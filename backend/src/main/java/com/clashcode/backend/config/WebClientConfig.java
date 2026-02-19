a98b32a12d7b33647e2731e0fc2cc6d1813e9321
kerolos samy <kerosamy42004@gmail.com>
2026-02-19 14:31:29 +0200

    [CLASHCODE-210] update matcing service

--- backend/src/main/java/com/clashcode/backend/config/WebClientConfig.java
+++ backend/src/main/java/com/clashcode/backend/config/WebClientConfig.java
@@ -1,5 +1,6 @@
 package com.clashcode.backend.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -8,9 +9,10 @@ import org.springframework.web.reactive.function.client.WebClient;
 @Configuration
 public class WebClientConfig {
     @Bean
-    public WebClient matchingWebClient(WebClient.Builder builder) {
+    public WebClient matchingWebClient(WebClient.Builder builder,
+                                       @Value("${MATCHING_SERVICE_URL}") String url) {
         return builder
-                .baseUrl("http://localhost:8081")
+                .baseUrl(url)
                 .build();
     }
 }
